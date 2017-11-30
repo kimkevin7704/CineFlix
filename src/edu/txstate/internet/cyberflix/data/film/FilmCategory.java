89ef8a594a38f57ab96a9220f01db85f0abbbd15
Ryan Jalufka <ryanjalufka@gmail.com>
2017-11-30 14:38:32 -0600

    Add dummy enum to ensure db validity

--- src/edu/txstate/internet/cyberflix/data/film/FilmCategory.java
+++ src/edu/txstate/internet/cyberflix/data/film/FilmCategory.java
@@ -1,6 +1,7 @@
 package edu.txstate.internet.cyberflix.data.film;
 
 public enum FilmCategory {
+	NONE, // this ensure the values line up with the data in the db (db starts at 1, not 0)
 	ACTION,
 	ANIMATION,
 	CHILDREN,
